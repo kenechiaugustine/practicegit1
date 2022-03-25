32ebe28144ccb4bf9b2056d0ddf7c6438dccb90f
Kene Arionye <kenechiaugustine@gmail.com>
2022-03-25 09:00:49 +0100

    first commit from james

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ console.log(`I'm in the kene branch`)
 
 
 
-console.log('Hello world')
+console.log('Hello world, by James')
 
 console.log('Kene made some change')
 
+
+
+
+console.log('Hi this is james branch and first commit')
+console.log('James changes and implementation')
